c406b8e7a3a9161cd84049b3b4fed20770503c65
Christopher Baptista <57831206+christopherbaptista@users.noreply.github.com>
2021-01-12 22:34:01 +0700

    Update GameStatus.java

--- Final Project/Snake/src/id/ac/its/kelompok/snakegame/GameStatus.java
+++ Final Project/Snake/src/id/ac/its/kelompok/snakegame/GameStatus.java
@@ -1,6 +1,6 @@
 package id.ac.its.kelompok.snakegame;
 
-public enum GameStatus
-{
+public enum GameStatus {
+    
     NOT_STARTED, RUNNING, PAUSED, GAME_OVER, SHOW_HIGHSCORE, SETTINGS, CREDITS,
 }
